dbc046ade42fee88ebd5ce1d50b6cc89ac4427e7
James Oladimeji <elderjames314@gmail.com>
2022-09-09 04:39:54 +0100

    print integer in java

---
+++ src/hundreds/PrintInteger02.java
@@ -0,0 +1,10 @@
+package hundreds;
+
+public class PrintInteger02 {
+    public static void main(String[] args) {
+        int num; //declare variable
+        for(num=1; num < 10; num++) {
+            System.out.println(num);
+        }
+    }
+}
